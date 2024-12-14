a92e08e41082532934c8212550e61e31cf89bfa4
BhargavGarge <bhargaavvv@gmail.com>
2024-12-14 17:22:46 +0530

    Done with Home and half cart page

--- ecomm-frontend/src/pages/cart.tsx
+++ ecomm-frontend/src/pages/cart.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
 
+const productPrice = 2500; // Product price
+const discount = 400; // Discount
+const shippingCharges = 200; // Shipping charges
+const subtotalAfterDiscount = productPrice - discount; // Subtotal after discount
+const tax = Math.round(subtotalAfterDiscount * 0.18); // Tax (18%)
+const total = subtotalAfterDiscount + tax + shippingCharges; // Total amount
+const cartItems = [];
 const Cart = () => {
-  return <div>Cart</div>;
+  const [couponCode, setCouponCode] = useState("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState(false);
+
+  useEffect(() => {
+    const timeOutID = setTimeout(() => {
+      if (Math.random() > 0.5) {
+        setIsValidCouponCode(true);
+      } else {
+        setIsValidCouponCode(false);
+      }
+    }, 1000);
+    return () => {
+      clearTimeout(timeOutID);
+      setIsValidCouponCode(false);
+    };
+  });
+
+  return (
+    <>
+      <div className="cart">
+        <main></main>
+        <aside>
+          <p>Product Price: ₹{productPrice}</p>
+          <p>Discount: ₹{discount}</p>
+          <p>Subtotal After Discount: ₹{subtotalAfterDiscount}</p>
+          <p>Tax (18% GST): ₹{tax}</p>
+          <p>Shipping Charges: ₹{shippingCharges}</p>
+          <p>
+            <b>Total: ₹{total}</b>
+          </p>
+          <input
+            type="text"
+            className="text"
+            value={couponCode}
+            onChange={(e) => setCouponCode(e.target.value)}
+            placeholder="Coupon Code"
+          />
+          {couponCode &&
+            (isValidCouponCode ? (
+              <span className="green">
+                {" "}
+                ₹{discount} off the using the <code>{couponCode}</code>
+              </span>
+            ) : (
+              <span className="red">
+                Invalid Coupon <VscError />
+              </span>
+            ))}
+        </aside>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
--- ecomm-frontend/src/pages/home.tsx
+++ ecomm-frontend/src/pages/home.tsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
+
 const Home = () => {
-  return <div>home</div>;
+  const addToCartHandler = () => {
+    console.log("Add to cart button clicked");
+  };
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard
+          productId="ert"
+          name="HeadPhone"
+          price={2500}
+          stock={34}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/I/713tn9TU1IL._SX522_.jpg"
+        />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
